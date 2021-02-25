d1e0d289cb5d07d0f33d19f5498a72a2aef9f106
farahminerva <farahashilaholii@gmail.com>
2021-02-25 21:30:02 +0700

    belajar php dasar

---
+++ belajar.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nama = 'Minerva' ;
+    $umur = 20;
+
+    echo 'Nama : ' . $nama;
+    echo '<br/>Umur : ' . $umur. ' Tahun'
+
+?>
---
+++ lingkaran.php
@@ -0,0 +1,10 @@
+<?php
+    define('PHI',3,14);
+    $jari_jari = 7;
+    $jari_jari =21;
+    $luas_lingkaran = PHI * $jari_jari * $jari_jari;
+    echo "luas lingkarang adalah : ".$luas_lingkaran;
+
+
+
+?>
